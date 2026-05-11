8d3995185b2845d22741309dbe33ce18af4631ab
Vincent Rist <vincent.rist@ovgu.de>
2026-05-11 12:41:24 +0200

    better naming

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import sys
 from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'nit_aprilcube'
-model_name = 'aprilcube'
+PKG_NAME = 'nit_aprilcube'
+MODEL_NAME = 'aprilcube'
 
 # --- Validation Logic ---
-tags_scale_path = os.path.join('models', model_name, 'meshes', 'tags_scaled')
+tags_scale_path = os.path.join('models', MODEL_NAME, 'meshes', 'tags_scaled')
 if not os.path.exists(tags_scale_path) or not os.listdir(tags_scale_path):
     print(f"ERROR: Texture directory is missing or empty: {tags_scale_path}", file=sys.stderr)
     print("Please run the 'scale_up_tags.py' script first to generate the scaled textures.", file=sys.stderr)
@@ -20,24 +20,24 @@ model_data_files = []
 for root, _, files in os.walk('models'):
     if files:
         # Construct the installation path: share/nit_aprilcube/models/...
-        target_dir = os.path.join('share', package_name, root)
+        target_dir = os.path.join('share', PKG_NAME, root)
         file_paths = [os.path.join(root, f) for f in files]
         model_data_files.append((target_dir, file_paths))
 
 setup(
-    name=package_name,
+    name=PKG_NAME,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+            ['resource/' + PKG_NAME]),
+        ('share/' + PKG_NAME, ['package.xml']),
 
         # launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', PKG_NAME, 'launch'), glob('launch/*.launch.py')),
 
         # config files
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', PKG_NAME, 'config'), glob('config/*')),
 
         # Merge the recursively found model files into data_files
     ] + model_data_files,
