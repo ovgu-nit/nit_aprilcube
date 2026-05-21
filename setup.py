31b5b486f9ae53937a4405dffd799372dfc292c8
vincirist <vincent.rist@ovgu.de>
2026-05-21 14:42:56 +0200

    conditional attributes in setup (dont share large simulation files)

--- setup.py
+++ setup.py
@@ -6,23 +6,30 @@ from setuptools import find_packages, setup
 PKG_NAME = 'nit_aprilcube'
 MODEL_NAME = 'aprilcube'
 
-# --- Validation Logic ---
-tags_scale_path = os.path.join('models', MODEL_NAME, 'meshes', 'tags_scaled')
-if not os.path.exists(tags_scale_path) or not os.listdir(tags_scale_path):
-    print(f"ERROR: Texture directory is missing or empty: {tags_scale_path}", file=sys.stderr)
-    print("Please run the 'scale_up_tags.py' script first to generate the scaled textures.", file=sys.stderr)
-    print("\tpython3 src/nit_aprilcube/scale_up_tags.py", file=sys.stderr)
+# Check if we are building for a simulation environment
+# Default to False so the real robot stays lightweight
+IS_SIM = os.environ.get('ROS_SIM', 'false').lower() == 'true'
 
-
-# --- Resource Collection ---
-# This helper collects all files in the models directory recursively
 model_data_files = []
-for root, _, files in os.walk('models'):
-    if files:
-        # Construct the installation path: share/nit_aprilcube/models/...
-        target_dir = os.path.join('share', PKG_NAME, root)
-        file_paths = [os.path.join(root, f) for f in files]
-        model_data_files.append((target_dir, file_paths))
+
+# Only validate and collect Gazebo/Simulation models if ROS_SIM is active
+if IS_SIM:
+    # --- Validation Logic ---
+    tags_scale_path = os.path.join('models', MODEL_NAME, 'meshes', 'tags_scaled')
+    if not os.path.exists(tags_scale_path) or not os.listdir(tags_scale_path):
+        print(f"ERROR: Texture directory is missing or empty: {tags_scale_path}", file=sys.stderr)
+        print("Please run the 'scale_up_tags.py' script first to generate the scaled textures.", file=sys.stderr)
+        print("\tpython3 src/nit_aprilcube/scale_up_tags.py", file=sys.stderr)
+
+    # --- Resource Collection ---
+    for root, _, files in os.walk('models'):
+        if files:
+            target_dir = os.path.join('share', PKG_NAME, root)
+            file_paths = [os.path.join(root, f) for f in files]
+            model_data_files.append((target_dir, file_paths))
+else:
+    pass
+    #print(f"[{PKG_NAME}] ROS_SIM not true. Skipping Gazebo model deployment for real robot.", file=sys.stderr)
 
 setup(
     name=PKG_NAME,
@@ -38,9 +45,7 @@ setup(
 
         # config files
         (os.path.join('share', PKG_NAME, 'config'), glob('config/*')),
-
-        # Merge the recursively found model files into data_files
-    ] + model_data_files,
+    ] + model_data_files, # Will be empty on the real robot
     
     install_requires=['setuptools', 'Pillow'],
     zip_safe=True,
