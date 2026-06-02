11edfd534e7d51858a2e96199aeb1326ade7af83
Vincent Rist <vincent.rist@ovgu.de>
2026-06-02 16:18:13 +0200

    cube posse estimation works

--- nit_aprilcube/aprilcube_detector.py
+++ nit_aprilcube/aprilcube_detector.py
@@ -96,7 +96,6 @@ class AprilcubeDetector(Node):
     def perceive(self):
         t_now = self.get_clock().now()
         new_poses = self._cube_pose_candidates()
-        self.get_logger().info(f'Received {len(new_poses)=}.')
 
         if len(new_poses) == 0:
             if self.cube_pose is None:
